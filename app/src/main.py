8b80ca33b62484ef01dbe3b9ef054c0fa13ed4ba
GTharunesh <122850848+GTharunesh@users.noreply.github.com>
2023-03-24 03:32:46 +0000

    html and mqtt js integration

--- app/src/main.py
+++ app/src/main.py
@@ -24,12 +24,25 @@ TRUNK_OPEN_STATUS_SUBSCRIPTION_TOPIC = "sampleApp/currentTrunkOpenStatus"
 
 GET_ALL_CURRENT_STATUS_TOPIC = "sampleapp/getAllStatus"
 
+subscribeTopic = {
+    'hood_status': "sampleApp/currentHoodOpenStatus",
+    'rear_rh_door_status': "sampleApp/currentDoorStateRearRH",
+    'rear_lh_door_status': "sampleApp/currentDoorStateRearLH",
+    'trunk_status': "sampleApp/currentTrunkOpenStatus",
+}
+
 
 class SampleApp(VehicleApp):
     def __init__(self, vehicle_client: Vehicle):
         # SampleApp inherits from VehicleApp.
         super().__init__()
         self.Vehicle = vehicle_client
+
+    async def mqttPublish(self, name: str, status):
+        await self.publish_event(
+            subscribeTopic[name],
+            json.dumps({"name": name, "status": status}),
+        )
 
     async def on_start(self):
         await self.Vehicle.Body.Hood.IsOpen.subscribe(self.on_hood_change)
@@ -43,61 +56,35 @@ class SampleApp(VehicleApp):
 
     async def on_hood_change(self, data: DataPointReply):
         hood_status = data.get(self.Vehicle.Body.Hood.IsOpen).value
-        await self.publish_mqtt_event(
-            HOOD_OPEN_STATUS_SUBSCRIPTION_TOPIC,
-            json.dumps({"hoodStatus": hood_status}),
-        )
+        await self.mqttPublish("hood_status", hood_status)
 
     async def on_trunk_change(self, data: DataPointReply):
         trunk_status = data.get(self.Vehicle.Body.Trunk.Rear.IsOpen).value
-        await self.publish_mqtt_event(
-            TRUNK_OPEN_STATUS_SUBSCRIPTION_TOPIC,
-            json.dumps({"trunkStatus": trunk_status}),
-        )
+        await self.mqttPublish("trunk_status", trunk_status)
 
     async def on_rear_rh_door_change(self, data: DataPointReply):
         rear_rh_door_status = data.get(self.Vehicle.Cabin.Door.Row2.Right.IsOpen).value
-        await self.publish_mqtt_event(
-            DOOR_STATE_REAR_RH_SUBSCRIPTION_TOPIC,
-            json.dumps({"rearRHDoorStatus": rear_rh_door_status}),
-        )
+        await self.mqttPublish("rear_rh_door_status", rear_rh_door_status)
 
     async def on_rear_lh_door_change(self, data: DataPointReply):
         rear_lh_door_status = data.get(self.Vehicle.Cabin.Door.Row2.Left.IsOpen).value
-        await self.publish_mqtt_event(
-            DOOR_STATE_REAR_LH_SUBSCRIPTION_TOPIC,
-            json.dumps({"rearLHDoorStatus": rear_lh_door_status}),
-        )
+        await self.mqttPublish("rear_lh_door_status", rear_lh_door_status)
 
     @subscribe_topic(GET_ALL_CURRENT_STATUS_TOPIC)
     async def on_get_all_current_status_request_received(self, data: str) -> None:
-        hood_status = (await self.Vehicle.Body.Hood.IsOpen.get()).value
-        trunk_status = (await self.Vehicle.Body.Trunk.Rear.IsOpen.get()).value
-        rear_rh_door_status = (
-            await self.Vehicle.Cabin.Door.Row2.Right.IsOpen.get()
-        ).value
-        rear_lh_door_status = (
-            await self.Vehicle.Cabin.Door.Row2.Right.IsOpen.get()
-        ).value
-
-        await self.publish_mqtt_event(
-            HOOD_OPEN_STATUS_SUBSCRIPTION_TOPIC,
-            json.dumps({"hoodStatus": hood_status}),
+        await self.mqttPublish(
+            "hood_status", (await self.Vehicle.Body.Hood.IsOpen.get()).value
         )
-
-        await self.publish_mqtt_event(
-            TRUNK_OPEN_STATUS_SUBSCRIPTION_TOPIC,
-            json.dumps({"trunkStatus": trunk_status}),
+        await self.mqttPublish(
+            "trunk_status", (await self.Vehicle.Body.Trunk.Rear.IsOpen.get()).value
         )
-
-        await self.publish_mqtt_event(
-            DOOR_STATE_REAR_RH_SUBSCRIPTION_TOPIC,
-            json.dumps({"rearRHDoorStatus": rear_rh_door_status}),
+        await self.mqttPublish(
+            "rear_rh_door_status",
+            (await self.Vehicle.Cabin.Door.Row2.Right.IsOpen.get()).value,
         )
-
-        await self.publish_mqtt_event(
-            DOOR_STATE_REAR_LH_SUBSCRIPTION_TOPIC,
-            json.dumps({"rearLHDoorStatus": rear_lh_door_status}),
+        await self.mqttPublish(
+            "rear_lh_door_status",
+            (await self.Vehicle.Cabin.Door.Row2.Right.IsOpen.get()).value,
         )
 
 
